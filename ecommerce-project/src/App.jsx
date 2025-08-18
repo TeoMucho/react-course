6c0fcc26bbc2eb08c24eb5ed7d2aa3c87a72598b
TeoMucho <teo.celikz@gmail.com>
2025-08-18 12:52:58 +0000

    Browser Router fix

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,4 @@
+import {Route, Routes} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
@@ -5,7 +6,10 @@ export function App() {
   
 
   return (
-   <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} ></Route>
+    </Routes>
+   
   )
 }
 
