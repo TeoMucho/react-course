f854c7d85c6c18b15c88d2a1ee1a8ffc3e240a70
TeoMucho <teo.celikz@gmail.com>
2025-08-21 08:43:32 +0000

    Header Component

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -7,33 +7,7 @@ export function HomePage () {
     <>
 <title>Ecommerce Project</title>
 
-      <header className="header">
-        <div className="left-section">
-          <Link to="/" className="header-link">
-            <img className="logo" src="/images/logo-white.png" alt="Logo" />
-            <img className="mobile-logo" src="/images/mobile-logo-white.png" alt="Logo mobil" />
-          </Link>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-          <button className="search-button">
-            <img className="search-icon" src="/images/icons/search-icon.png" alt="Search" />
-          </button>
-        </div>
-
-        <div className="right-section">
-         <Link className="orders-link header-link" to="/orders">
-            <span className="orders-text">Orders</span>
-          </Link>
-
-          <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="/images/icons/cart-icon.png" alt="Cart" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </Link>
-        </div>
-      </header>
+   
 
     <div className="home-page">
       <div className="products-grid">
